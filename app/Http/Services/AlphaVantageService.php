a00f16b706a48e64f57efdc29ef703d85e898b04
Hamid Ghorashi <h.ghorashi@gmail.com>
2024-10-19 20:05:55 +0400

    Fetch data through Vantage API

---
+++ app/Http/Services/AlphaVantageService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Services;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use Illuminate\Cache\CacheManager;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+
+class AlphaVantageService
+{
+    public function __construct(
+        protected Client $httpClient,
+        protected CacheManager $cache
+    ) {}
+
+    public function getStockData(string $symbol, int $interval = 5): Collection
+    {
+        // Define the cache key based on the symbol and interval
+        $cacheKey = 'stock_intraday_' . md5($symbol . $interval);
+
+        // Try to get the data from cache or make an API request
+        return $this->cache->remember($cacheKey, $interval, function () use ($symbol, $interval) {
+            $queryString = $this->buildQueryString([
+                'function' => 'TIME_SERIES_INTRADAY',
+                'symbol'   => $symbol,
+                'interval' => $interval . 'min',
+            ]);
+
+            try {
+                $response = $this->httpClient->get('', $queryString);
+                $data     = json_decode($response->getBody(), true);
+
+                throw_if(JSON_ERROR_NONE !== json_last_error(), new \Exception('Could not extract data from the API response.'));
+            } catch (\Throwable $th) {
+                Log::error('AlphaVantage API Error: ' . $th->getMessage());
+
+                collect();
+            }
+
+            return collect($data['Time Series (' . $interval . 'min)'] ?? []);
+        });
+    }
+
+    private function buildQueryString(array $params): array
+    {
+        return [RequestOptions::QUERY => [
+            'apikey' => config('alpha_vantage.apiKey'),
+        ] + $params];
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Http\Services\AlphaVantageService;
+use GuzzleHttp\Client as HttpClient;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,14 +14,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(AlphaVantageService::class, function (Application $app) {
+            return new AlphaVantageService(new HttpClient([
+                'base_uri' => config('alpha_vantage.url'),
+            ]), $app->make('cache'));
+        });
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
-    }
+    public function boot(): void {}
 }
