61a09cdac9dd581575b00bb5dcb786c267350a5e
Hamid Ghorashi <h.ghorashi@gmail.com>
2024-10-20 23:47:08 +0400

    Run seeder from the interface

--- app/Http/Controllers/FetchStockDataController.php
+++ app/Http/Controllers/FetchStockDataController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\FetchStockData;
+use Illuminate\Support\Facades\Artisan;
 
 class FetchStockDataController extends Controller
 {
@@ -17,6 +18,7 @@ class FetchStockDataController extends Controller
 
     public function run()
     {
+        Artisan::call('db:seed', ['--class' => 'StockSymbolsSeeder']);
         $this->job->handle();
 
         return response()->redirectToRoute('home');
